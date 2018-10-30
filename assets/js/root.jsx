6bb9a7476f42763b211a0ee57d74c864ded1454a
Nat Tuck <nat@ferrus.net>
2018-10-30 00:09:26 -0400

    Fetch user data.

--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import $ from 'jquery';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function root_init(node) {
   let prods = window.products;
@@ -14,6 +15,7 @@ class Root extends React.Component {
     super(props);
     this.state = {
       products: props.products,
+      users: [],
     };
 
     //this.fetch_products();
@@ -32,18 +34,44 @@ class Root extends React.Component {
     });
   }
 
+  fetch_users() {
+    $.ajax("/api/v1/users", {
+      method: "get",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: "",
+      success: (resp) => {
+        let state1 = _.assign({}, this.state, { users: resp.data });
+        this.setState(state1);
+      },
+    });
+  }
+
   render() {
     return <div>
-      <Header />
-      <ProductList products={this.state.products} />
+      <Router>
+        <div>
+          <Header root={this} />
+          <Route path="/" exact={true} render={() =>
+            <ProductList products={this.state.products} />
+          } />
+          <Route path="/users" exact={true} render={() =>
+            <UserList users={this.state.users} />
+          } />
+        </div>
+      </Router>
     </div>;
   }
 }
 
-function Header(_props) {
+function Header(props) {
+  let {root} = props;
   return <div className="row my-2">
-    <div className="col-6">
-      <h1>Husky Shop</h1>
+    <div className="col-4">
+      <h1><Link to={"/"}>Husky Shop</Link></h1>
+    </div>
+    <div className="col-2">
+      <p><Link to={"/users"} onClick={root.fetch_users.bind(root)}>Users</Link></p>
     </div>
     <div className="col-6">
       <div className="form-inline my-2">
@@ -56,7 +84,7 @@ function Header(_props) {
 }
 
 function ProductList(props) {
-  let prods = _.map(props.products, (p) => <Product key={p.id} product={p} />);
+  let prods = _.map(props.products, (pp) => <Product key={pp.id} product={pp} />);
   return <div className="row">
     {prods}
   </div>;
@@ -67,9 +95,38 @@ function Product(props) {
   return <div className="card col-4">
     <div className="card-body">
       <h2 className="card-title">{product.name}</h2>
-      <p className="card-text">{product.desc} <br/>
-      price: {product.price}</p>
+      <p className="card-text">
+        {product.desc} <br/>
+        price: {product.price}
+      </p>
     </div>
   </div>;
+}
+
+function UserList(props) {
+  let rows = _.map(props.users, (uu) => <User key={uu.id} user={uu} />);
+  return <div className="row">
+    <div className="col-12">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>email</th>
+            <th>admin?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  </div>;
+}
+
+function User(props) {
+  let {user} = props;
+  return <tr>
+    <td>{user.email}</td>
+    <td>{user.admin ? "yes" : "no"}</td>
+  </tr>;
 }
 
