380e2b8143b8eae0e17b1d6b15c41d3019f83601
Nat Tuck <nat@ferrus.net>
2018-10-29 20:43:06 -0400

    Show products.

--- lib/husky_shop_web/controllers/page_controller.ex
+++ lib/husky_shop_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule HuskyShopWeb.PageController do
   use HuskyShopWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    products = HuskyShop.Products.list_products()
+    |> Enum.map(&(Map.take(&1, [:id, :name, :desc, :price])))
+    render conn, "index.html", products: products
   end
 end
