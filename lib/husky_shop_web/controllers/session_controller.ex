0fbecf892b0c87febc901882515008546fbee365
Nat Tuck <nat@ferrus.net>
2018-10-30 00:58:55 -0400

    Create session controller.

---
+++ lib/husky_shop_web/controllers/session_controller.ex
@@ -0,0 +1,17 @@
+defmodule HuskyShopWeb.SessionController do
+  use HuskyShopWeb, :controller
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    with %User{} = user <- get_and_auth_user(email, pass) do
+      resp = %{
+        data: %{
+          token: Phoenix.Token.sign(HuskyShopWeb.Endpoint, "user_id", user.id),
+          user_id: user.id,
+        }
+      }
+      conn
+      |> put_resp_header("content-type", "application/json; charset=utf-8")
+      |> send_resp(:created, Jason.encode!(resp))
+    end
+  end
+end
