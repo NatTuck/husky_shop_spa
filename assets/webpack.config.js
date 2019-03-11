7001f689867270ea1a1ffeea58b66a7892181fa5
Nat Tuck <Nat Tuck>
2019-03-11 14:25:10 -0400

    Fix .babelrc nonsense.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env, options) => ({
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/env', '@babel/react'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         }
       },
--- lib/husky_shop_web/controllers/page_controller.ex
+++ lib/husky_shop_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule HuskyShopWeb.PageController do
   use HuskyShopWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    products = HuskyShop.Products.list_products()
+    |> Enum.map(fn prod ->
+      HuskyShopWeb.ProductView.render("product.json", %{product: prod})
+    end)
+    IO.inspect(products)
+    render conn, "index.html", products: products
   end
 end
